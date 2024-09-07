f652af8f2609b63bc1b4b0fd9f4da7c4f05c105c
Somto Ufondu <somtoufondu@gmail.com>
2024-09-07 15:14:59 -0600

    Implement user authentication using bcrypt: Add user registration, login, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   get 'about', to: 'about#index'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   root to: 'products#index'
 
